61c2f038004a36c43c83fed087a2814e97a3ab85
Ivan Markelov <markelov.vanya2016@yandex.ru>
2026-06-21 19:48:05 +0300

    доработан

--- fit_life.py
+++ fit_life.py
@@ -6,17 +6,40 @@ ML_LITER = 1000
 print("Добро пожаловать в фитнес-трекер от FitLife")
 
 # 1. Знакомство
-user_name = input("Введите имя: ")
-user_name = user_name.title()
-user_age = input("Введите возраст: ")
-user_age_int = int(user_age)
-
+while True:
+    user_name = input("Введите имя: ").strip()
+    if user_name != "":
+        user_name = user_name.title()
+        break
+    print("Имя не может быть пустым или пробелом!")
+while True:
+    user_age = input("Введите возраст: ").strip()
+    try:
+        user_age_int = int(user_age)
+        if user_age_int > 0:
+            break
+        print("Возраст должен быть больше нуля!")
+    except ValueError:
+        print("Укажите возраст в цифрах!")
 # 2. Сбор данных
-user_weight = input("Введите вес в кг (например, 75): ")
-user_weight_float = float(user_weight)
-user_height = input("Введите рост в м (используя точку,например 1.75): ")
-user_height_float = float(user_height)
-
+while True:
+    user_weight = input("Введите вес в кг (пример, 75): ").strip()
+    try:
+        user_weight_float = float(user_weight)
+        if user_weight_float > 0:
+            break
+        print("Вес должен быть больше нуля!")
+    except ValueError:
+        print("Укажите вес в цифрах (например, 75)")
+while True:
+    user_height = input("Введите рост в м (c точкой,например 1.75): ").strip()
+    try:
+        user_height_float = float(user_height)
+        if user_height_float > 0:
+            break
+        print("Рост должен быть больше нуля!")
+    except ValueError:
+        print("Укажите рост в цифрах (используя точку,например 1.75)")
 # 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
 # расчёт индекса массы тела
 bmi = user_weight_float / (user_height_float ** 2)
@@ -28,6 +51,14 @@ water_l = water_ml / ML_LITER
 # 4. Вывод красивого результата
 print(f"Отчет для пользователя: {user_name} ({user_age_int} г.)")
 print(f"Твой Индекс Массы Тела: {bmi}")
+if bmi < 18.5:
+    print("Дефицит массы тела")
+elif bmi < 25.0:
+    print("Нормальный вес")
+elif bmi < 30.0:
+    print("Избыточный вес")
+else:
+    print("Ожирение")
 print(f"Рекомендуемая норма воды: {water_l:.1f} л. в день")
 print()
 print("Расчет окончен. Будьте здоровы!")
